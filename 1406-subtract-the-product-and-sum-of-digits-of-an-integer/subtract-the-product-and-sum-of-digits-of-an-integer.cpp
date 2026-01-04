df25d23e32ba5d00a6daa8348129d5e5edf54da9
Aman Chaurasia <129844474+aman18Chaurasia@users.noreply.github.com>
2026-01-04 19:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (54.45%) - LeetSync

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -3,11 +3,12 @@ public:
     int subtractProductAndSum(int n) {
         int s=0;
         int m=1;
+        int d;
         while(n>0){
             int d=n%10;
-            s=s+d;
-            m=m*d;
-            n=n/10;
+            s+=d;
+            m*=d;
+            n/=10;
         }
         return m-s;
     }
