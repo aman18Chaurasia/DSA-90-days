d2fb2d5bd4b2afec89f8b87eb295272825a09794
Aman Chaurasia <129844474+aman18Chaurasia@users.noreply.github.com>
2026-01-10 22:55:54 +0530

    Time: 7 ms (96.05%) | Memory: 9.7 MB (92.39%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int minimumDeleteSum(string a, string b) {
-        int x = a.size(), y = b.size();
-        vector<vector<int>> t(x + 1, vector<int>(y + 1));
+        int n = a.size(), m = b.size();
+        vector<int> d(m + 1);
 
-        for (int i = x - 1; i >= 0; i--)
-            t[i][y] = t[i + 1][y] + a[i];
+        for (int j = m - 1; j >= 0; j--)
+            d[j] = d[j + 1] + b[j];
 
-        for (int j = y - 1; j >= 0; j--)
-            t[x][j] = t[x][j + 1] + b[j];
-
-        for (int i = x - 1; i >= 0; i--) {
-            for (int j = y - 1; j >= 0; j--) {
+        for (int i = n - 1; i >= 0; i--) {
+            int p = d[m];
+            d[m] += a[i];
+            for (int j = m - 1; j >= 0; j--) {
+                int q = d[j];
                 if (a[i] == b[j])
-                    t[i][j] = t[i + 1][j + 1];
+                    d[j] = p;
                 else
-                    t[i][j] = min(a[i] + t[i + 1][j], b[j] + t[i][j + 1]);
+                    d[j] = min(a[i] + d[j], b[j] + d[j + 1]);
+                p = q;
             }
         }
-        return t[0][0];
+        return d[0];
     }
 };
