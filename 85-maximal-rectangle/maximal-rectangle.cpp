d651b3362a3469fcbc715bf03deab47c735faac3
Aman Chaurasia <129844474+aman18Chaurasia@users.noreply.github.com>
2026-01-11 19:41:00 +0530

    Time: 4 ms (74.10%) | Memory: 17.9 MB (75.35%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& m) {
-        if (m.empty()) return 0;
-
-        int r = m.size(), c = m[0].size(), ans = 0;
-        vector<int> h(c, 0);
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (m[i][j] == '1') h[j]++;
-                else h[j] = 0;
-            }
-            ans = max(ans, solve(h));
-        }
-        return ans;
-    }
-
-    int solve(vector<int>& h) {
+    int largest(vector<int>& h) {
         stack<int> st;
+        int ans = 0;
         h.push_back(0);
-        int res = 0;
 
         for (int i = 0; i < h.size(); i++) {
             while (!st.empty() && h[st.top()] > h[i]) {
                 int x = h[st.top()];
                 st.pop();
                 int w = st.empty() ? i : i - st.top() - 1;
-                res = max(res, x * w);
+                ans = max(ans, x * w);
             }
             st.push(i);
         }
         h.pop_back();
+        return ans;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int r = matrix.size(), c = matrix[0].size();
+        vector<int> h(c, 0);
+        int res = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (matrix[i][j] == '1') h[j]++;
+                else h[j] = 0;
+            }
+            res = max(res, largest(h));
+        }
         return res;
     }
 };
