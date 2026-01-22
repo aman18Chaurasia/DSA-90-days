9838519090177a729fcdb9e9cb5361fcf5da00ee
Aman Chaurasia <129844474+aman18Chaurasia@users.noreply.github.com>
2026-01-22 16:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (5.95%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int freq[2001] = {0};
+        bool seen[1001] = {false};
+
+        for (int x : arr) {
+            freq[x + 1000]++;
+        }
+
+        for (int i = 0; i < 2001; i++) {
+            if (freq[i] > 0) {
+                if (seen[freq[i]]) return false;
+                seen[freq[i]] = true;
+            }
+        }
+
+        return true;
+    }
+};
